0220427f980725f118a72ca3fb77b9a6a581e627
Isaac Orraiz <isaacorraiz@gmail.com>
2023-09-23 23:31:47 -0400

    Finished Show details

--- js/script.js
+++ js/script.js
@@ -163,6 +163,73 @@ async function displayMovieDetails() {
   document.getElementById("movie-details").appendChild(div);
 }
 
+//TV Shows details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+  const show = await fetchDataMoviesApi(`tv/${showId}`);
+  displayBackgroundImage("tvShow", show.backdrop_path);
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="details-top">
+          <div>
+          ${
+            show.poster_path
+              ? `<img
+                      src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+                      class="card-img-top"
+                      alt="${show.name}"
+                  />`
+              : `<img
+                      src="../images/no-image.jpg"
+                      class="card-img-top"
+                      alt="No image"
+                  />`
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+            ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies
+            .map((companie) => companie.name)
+            .join(", ")}
+          </div>
+        </div>
+    `;
+
+  document.getElementById("show-details").appendChild(div);
+}
+
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement("div");
   overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
@@ -179,8 +246,8 @@ function displayBackgroundImage(type, backgroundPath) {
 
   if (type === "movie") {
     document.getElementById("movie-details").appendChild(overlayDiv);
-  } else if (type === "tvShow") {
-    document.getElementById("popular-shows").appendChild(overlayDiv);
+  } else {
+    document.getElementById("show-details").appendChild(overlayDiv);
   }
 }
 
@@ -227,6 +294,7 @@ function init() {
       break;
     case "/tv-details.html":
       console.log("TV details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
